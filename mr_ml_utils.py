9a31332ff78175456bfe387afb25313999924ca2
vadibekyan <adbvardan@gmail.com>
2023-07-10 13:41:33 +0100

    Add mr_ml_utils, modify mr_ml and mr_ml.ipynb

--- mr_ml_utils.py
+++ mr_ml_utils.py
@@ -51,8 +51,6 @@ def bcv_from_teff(teff):
     return bcv
 
 
-
-
 def Vmag_to_L(teff, v, plx):
     """
     Convert the visual magnitude (Vmag) of a star to its luminosity (L).
@@ -141,7 +139,7 @@ def Teq_from_teff_v_plx_a(teff, v, plx, a):
 
 
 
-def Teq_from_teff_v_plx_a(teff, v, plx, P, Mstar):
+def Teq_from_teff_v_plx_P(teff, v, plx, P, Mstar):
     """
     Calculate the APPROXIMATE equilibrium temperature (Teq) of a planet based on the effective temperature (teff) of its host star,
     the visual magnitude (v) of the star, the parallax (plx), and the semimajor axis (a) of the planet's orbit.
@@ -197,8 +195,74 @@ def Teq_from_L_a(L, a):
     return Teq
 
 
-if __name__ == "__main__":
-    L = 1
-    a = 0.2
-    print (Teq_from_L_a(L, a))
-    print (a)
+def teq_hellper_function(x, pl_orbsmax, pl_orbper, st_teff, sy_vmag, sy_plx, st_mass, Teq):
+    """
+    Helper function to calculate the equilibrium temperature (Teq) based on different parameters.
+
+    Parameters:
+        x (array-like): Input array or DataFrame row containing the values of the parameters.
+        pl_orbsmax (str): Column name or key for the planet's semimajor axis parameter.
+        pl_orbper (str): Column name or key for the planet's orbital period parameter.
+        st_teff (str): Column name or key for the stellar effective temperature parameter.
+        sy_vmag (str): Column name or key for the system's visual magnitude parameter.
+        sy_plx (str): Column name or key for the system's parallax parameter.
+        st_mass (str): Column name or key for the stellar mass parameter.
+        Teq (str): Column name or key for the equilibrium temperature (output) parameter.
+
+    Returns:
+        x (array-like): Input array or DataFrame row with the equilibrium temperature (Teq) added as a new column.
+
+    Note:
+        The function uses the `Teq_from_teff_v_plx_a` and `Teq_from_teff_v_plx_P` functions to calculate Teq.
+        The resulting Teq value is rounded to the nearest integer using `np.round`.
+    """
+    if x[pl_orbsmax] > 0:
+        # Calculate Teq using Teq_from_teff_v_plx_a function
+        x[Teq] = Teq_from_teff_v_plx_a(x[st_teff], x[sy_vmag], x[sy_plx], x[pl_orbsmax])
+    else:
+        # Calculate Teq using Teq_from_teff_v_plx_P function
+        x[Teq] = Teq_from_teff_v_plx_P(x[st_teff], x[sy_vmag], x[sy_plx], x[pl_orbper], x[st_mass])
+    
+    # Round the Teq value to the nearest integer
+    x[Teq] = np.round(x[Teq])
+    
+    return x
+
+
+def creating_MR_ML_table(nea_full_table):
+    """
+    Create a table with relevant columns for mass-radius relation analysis and calculate the equilibrium temperature (Teq).
+
+    Parameters:
+        nea_full_table (DataFrame): Full table containing the data.
+
+    Returns:
+        nea_MR_final_table (DataFrame): Final table with relevant columns: 'pl_rade', 'pl_bmasse', 'Teq'.
+    """
+    # Select relevant columns from the full table
+    relevant_columns = ['pl_rade', 'pl_orbsmax', 'pl_bmasse', 'pl_orbper', 'sy_vmag', 'sy_plx', 'st_teff', 'st_mass']
+    nea_relevant = nea_full_table[relevant_columns]
+
+    # Filter rows with positive values for mass and radius
+    nea_with_M_and_R = nea_relevant[(nea_relevant.pl_bmasse > 0) & (nea_relevant.pl_rade > 0)].reset_index(drop=True)
+
+    # Remove stars with negative parallax
+    nea_with_M_and_R = nea_with_M_and_R[nea_with_M_and_R.sy_plx > 0]
+
+    # Remove rows with NaN values except for 'pl_orbsmax' column
+    nea_with_M_and_R_cleaned = nea_with_M_and_R.dropna(subset=nea_with_M_and_R.columns.drop('pl_orbsmax'))
+
+    # Add 'Teq' column and initialize with 0
+    nea_with_M_and_R_cleaned['Teq'] = 0
+
+    # Calculate the equilibrium temperature 'Teq' using a helper function
+    nea_with_M_and_R_cleaned = nea_with_M_and_R_cleaned.apply(lambda x: teq_hellper_function(x, 'pl_orbsmax', 'pl_orbper', 'st_teff', 'sy_vmag', 'sy_plx', 'st_mass', 'Teq'), axis=1)
+
+    # Select the final columns for the output table and reset the index
+    nea_MR_final_table = nea_with_M_and_R_cleaned[['pl_rade', 'pl_bmasse', 'Teq']].reset_index(drop=True)
+
+    return nea_MR_final_table
+
+
+
+
